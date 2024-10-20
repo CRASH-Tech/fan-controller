b3f4b6a52ac3d2df5145e31df8ccd69091a55145
s.kharlamov <s.kharlamov@comagic.dev>
2024-10-20 15:25:23 +0000

    init

---
+++ arduino/src/main.cpp
@@ -0,0 +1,141 @@
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+#define FAN1_PWM_PIN 12     // D6
+#define FAN1_SIGNAL_PIN 13  // D7
+#define FAN2_PWM_PIN 0      // D3
+#define FAN2_SIGNAL_PIN 2   // D4
+#define FAN3_PWM_PIN 5      // D1
+#define FAN3_SIGNAL_PIN 4   // D2
+#define FAN4_PWM_PIN 15     // D8
+#define FAN4_SIGNAL_PIN 14  // D5
+
+#define EMERGENCY_SPEED 100
+
+unsigned long lastWatchdogReceived = 0;
+const unsigned long WATCHDOG_PERIOD = 30000;
+
+int getFanSpeedRpm(int SIGNAL_PIN) {
+  int highTime = pulseIn(SIGNAL_PIN, HIGH);
+  int lowTime = pulseIn(SIGNAL_PIN, LOW);
+  int period = highTime + lowTime;
+  if (period == 0) {
+    return 0;
+  }
+  float freq = 1000000.0 / (float)period;
+  return (freq * 60.0) / 2.0;
+}
+
+void setFanSpeedPercent(int PWM_PIN, int p) {
+  int value = (p / 100.0) * 255;
+  analogWrite(PWM_PIN, value);
+}
+
+void emergencyMode() {
+  Serial.println("EMERGENCY MODE!");
+
+  setFanSpeedPercent(FAN1_PWM_PIN, EMERGENCY_SPEED);
+  setFanSpeedPercent(FAN2_PWM_PIN, EMERGENCY_SPEED);
+  setFanSpeedPercent(FAN3_PWM_PIN, EMERGENCY_SPEED);
+  setFanSpeedPercent(FAN4_PWM_PIN, EMERGENCY_SPEED);
+}
+
+void resetWatchdog() { lastWatchdogReceived = millis(); }
+
+void checkWatchdog() {
+  unsigned long currentTime = millis();
+  if (currentTime - lastWatchdogReceived >= WATCHDOG_PERIOD) {
+    emergencyMode();
+    lastWatchdogReceived = millis();
+  }
+}
+
+void processCommand(String cmd) {
+  String command = cmd.substring(0, cmd.indexOf(' '));
+
+  if (command == "WATCHDOG") {
+    resetWatchdog();
+    return;
+  }
+
+  if (command == "SET") {
+    int fan = cmd.substring(cmd.indexOf(' ') + 1, cmd.lastIndexOf(' ')).toInt();
+    int pwm = cmd.substring(cmd.lastIndexOf(' ') + 1).toInt();
+    int pin;
+    switch (fan) {
+      case 1:
+        pin = FAN1_PWM_PIN;
+        break;
+      case 2:
+        pin = FAN2_PWM_PIN;
+        break;
+      case 3:
+        pin = FAN3_PWM_PIN;
+        break;
+      case 4:
+        pin = FAN4_PWM_PIN;
+        break;
+      default:
+        Serial.println("UNKNOWN FAN!");
+        return;
+    }
+
+    setFanSpeedPercent(pin, pwm);
+    Serial.printf("SET FAN %d TO %d%%\n", fan, pwm);
+    return;
+  }
+
+  if (command == "GET") {
+    int fan = cmd.substring(cmd.lastIndexOf(' ') + 1).toInt();
+    int pin;
+    switch (fan) {
+      case 1:
+        pin = FAN1_SIGNAL_PIN;
+        break;
+      case 2:
+        pin = FAN2_SIGNAL_PIN;
+        break;
+      case 3:
+        pin = FAN3_SIGNAL_PIN;
+        break;
+      case 4:
+        pin = FAN4_SIGNAL_PIN;
+        break;
+      default:
+        Serial.println("UNKNOWN FAN!");
+        return;
+    }
+
+    int rpm = getFanSpeedRpm(pin);
+    Serial.println(rpm);
+    return;
+  }
+
+  Serial.println("UNKNOWN COMMAND!");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(500);
+  Serial.print("STARTING...\n");
+  pinMode(FAN1_PWM_PIN, OUTPUT);
+  pinMode(FAN1_SIGNAL_PIN, INPUT_PULLUP);
+  pinMode(FAN2_PWM_PIN, OUTPUT);
+  pinMode(FAN2_SIGNAL_PIN, INPUT_PULLUP);
+  pinMode(FAN3_PWM_PIN, OUTPUT);
+  pinMode(FAN3_SIGNAL_PIN, INPUT_PULLUP);
+  pinMode(FAN4_PWM_PIN, OUTPUT);
+  pinMode(FAN4_SIGNAL_PIN, INPUT_PULLUP);
+
+  emergencyMode();
+}
+
+void loop() {
+  checkWatchdog();
+
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+    processCommand(cmd);
+  }
+}
---
+++ backend/main.go
@@ -0,0 +1,128 @@
+// package main
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"go.bug.st/serial"
+)
+
+var (
+	port serial.Port
+)
+
+func init() {
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+
+	var err error
+	port, err = serial.Open("/dev/ttyUSB0", mode)
+	if err != nil {
+		panic(err)
+	}
+
+	// port = aport
+}
+
+func main() {
+	// speeds := make(map[int]int)
+
+	// for i := 1; i <= 4; i++ {
+	// 	rpm, err := getFanSpeed(i)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	speeds[i] = rpm
+	// 	time.Sleep(2 * time.Second)
+	// }
+
+	// for fan, rpm := range speeds {
+	// 	fmt.Printf("FAN %d: %d\n", fan, rpm)
+	// }
+
+	var err error
+	err = setFanSpeed(1, 30)
+	if err != nil {
+		panic(err)
+	}
+	err = setFanSpeed(2, 30)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		err := watchdog()
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(10 * time.Second)
+	}
+
+	// rpm, err := getFanSpeed(1)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(rpm)
+
+}
+
+func watchdog() error {
+	_, err := port.Write([]byte("WATCHDOG\n"))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setFanSpeed(id, percent int) error {
+	//fmt.Printf("SET FAN %d TO %d\n", id, percent)
+
+	_, err := port.Write([]byte(fmt.Sprintf("SET %d %d\n", id, percent)))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getFanSpeed(id int) (int, error) {
+	//fmt.Printf("GET FAN %d SPEED\n", id)
+	_, err := port.Write([]byte(fmt.Sprintf("GET %d\n", id)))
+	if err != nil {
+		return -1, err
+	}
+
+	buff := make([]byte, 100)
+	for {
+		err := port.ResetInputBuffer()
+		if err != nil {
+			return -1, err
+		}
+
+		err = port.ResetOutputBuffer()
+		if err != nil {
+			return -1, err
+		}
+
+		port.SetReadTimeout(5 * time.Second)
+		n, err := port.Read(buff)
+		if err != nil {
+			return -1, err
+		}
+		if n == 0 {
+			break
+		}
+
+		//fmt.Printf("%v", string(buff[:n]))
+		data := strings.TrimSpace(string(buff[:n]))
+		rpm, err := strconv.Atoi(data)
+		return rpm, err
+	}
+
+	return -1, err
+}
